1153969ff3b11cd54e77568a993fba807de87b37
harshithva <harshith11032001@gmail.com>
2021-04-20 18:25:31 +0530

    Record & clear

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from db import Database
+from datetime import datetime
 
 db = Database('store.db')
 
 
 def populate_list(self):
     # delete all
-    for i in self.treeview1.delete(0, END)
+    for i in self.treeview1.get_children():
+        self.treeview1.delete(i)
+    # populate
     for row in db.fetch():
         self.treeview1.insert("", 'end',
                               values=row)
@@ -17,61 +20,70 @@ class ActivitesApp:
     def __init__(self, master=None):
         # build ui
         self.toplevel1 = tk.Tk() if master is None else tk.Toplevel(master)
-        self.frame2 = ttk.Frame(self.toplevel1)
-        self.label1 = ttk.Label(self.frame2)
-        self.label1.configure(text='Date')
-        self.label1.grid(column='0', pady='20', row='0')
-        self.entry1 = ttk.Entry(self.frame2)
-        _text_ = '''2021-04-20'''
-        self.entry1.delete('0', 'end')
-        self.entry1.insert('0', _text_)
-        self.entry1.grid(column='0', row='1')
-        self.label2 = ttk.Label(self.frame2)
-        self.label2.configure(text='Symbol')
-        self.label2.grid(column='1', row='0')
-        self.entry2 = ttk.Entry(self.frame2)
-        self.entry2.configure(exportselection='true')
-        _text_ = '''Symbol'''
-        self.entry2.delete('0', 'end')
-        self.entry2.insert('0', _text_)
-        self.entry2.grid(column='1', row='1')
-        self.label3 = ttk.Label(self.frame2)
-        self.label3.configure(text='Transaction Type')
-        self.label3.grid(column='2', row='0')
-        self.combobox1 = ttk.Combobox(self.frame2)
-        self.combobox1.configure(values='BUY SELL')
-        self.combobox1.grid(column='2', row='1')
-        self.label4 = ttk.Label(self.frame2)
-        self.label4.configure(text='Quantity')
-        self.label4.grid(column='3', row='0')
-        self.entry3 = ttk.Entry(self.frame2)
-        _text_ = '''0'''
-        self.entry3.delete('0', 'end')
-        self.entry3.insert('0', _text_)
-        self.entry3.grid(column='3', row='1')
-        self.label5 = ttk.Label(self.frame2)
-        self.label5.configure(text='Price')
-        self.label5.grid(column='4', row='0')
-        self.entry4 = ttk.Entry(self.frame2)
-        _text_ = '''0'''
-        self.entry4.delete('0', 'end')
-        self.entry4.insert('0', _text_)
-        self.entry4.grid(column='4', row='1')
+        self.frame1 = ttk.Frame(self.toplevel1)
+        self.date_label = ttk.Label(self.frame1)
+        self.date_label.configure(text='Date')
+        self.date_label.grid(column='0', pady='20', row='0')
 
-        self.record_button = ttk.Button(self.frame2)
+        self.date_entry = ttk.Entry(self.frame1)
+        _text_ = datetime.today().strftime('%Y-%m-%d')
+        self.date_entry.delete('0', 'end')
+        self.date_entry.insert('0', _text_)
+        self.date_entry.grid(column='0', row='1')
+
+        self.symbol_label = ttk.Label(self.frame1)
+        self.symbol_label.configure(text='Symbol')
+        self.symbol_label.grid(column='1', row='0')
+
+        self.symbol_entry = ttk.Entry(self.frame1)
+        self.symbol_entry.configure(exportselection='true')
+        _text_ = '''Symbol'''
+        self.symbol_entry.delete('0', 'end')
+        self.symbol_entry.insert('0', _text_)
+        self.symbol_entry.grid(column='1', row='1')
+
+        self.transaction_type_label = ttk.Label(self.frame1)
+        self.transaction_type_label.configure(text='Transaction Type')
+        self.transaction_type_label.grid(column='2', row='0')
+
+        self.transaction_type_combobox = ttk.Combobox(self.frame1)
+        self.transaction_type_combobox.configure(values='BUY SELL')
+        self.transaction_type_combobox.grid(column='2', row='1')
+
+        self.quantity_label = ttk.Label(self.frame1)
+        self.quantity_label.configure(text='Quantity')
+        self.quantity_label.grid(column='3', row='0')
+
+        self.quantity_entry = ttk.Entry(self.frame1)
+        _text_ = '''0'''
+        self.quantity_entry.delete('0', 'end')
+        self.quantity_entry.insert('0', _text_)
+        self.quantity_entry.grid(column='3', row='1')
+
+        self.price_label = ttk.Label(self.frame1)
+        self.price_label.configure(text='Price')
+        self.price_label.grid(column='4', row='0')
+
+        self.price_entry = ttk.Entry(self.frame1)
+        _text_ = '''0'''
+        self.price_entry.delete('0', 'end')
+        self.price_entry.insert('0', _text_)
+        self.price_entry.grid(column='4', row='1')
+
+        self.record_button = ttk.Button(self.frame1)
         self.record_button.configure(text='Record')
         self.record_button.grid(column='0', columnspan='3', pady='20', row='3')
         self.record_button.configure(command=self.record)
-        self.clear_button = ttk.Button(self.frame2)
+        self.clear_button = ttk.Button(self.frame1)
         self.clear_button.configure(text='Clear')
         self.clear_button.grid(column='1', columnspan='3', row='3')
         self.clear_button.configure(command=self.clear)
-        self.search_button = ttk.Button(self.frame2)
+        self.search_button = ttk.Button(self.frame1)
         self.search_button.configure(text='Search')
         self.search_button.grid(column='2', columnspan='3', row='3')
         self.search_button.configure(command=self.search)
 
-        self.treeview1 = ttk.Treeview(self.frame2)
+        self.treeview1 = ttk.Treeview(self.frame1)
         self.treeview1_cols = ['column1', 'column2',
                                'column3', 'column4', 'column5', 'column6']
         self.treeview1_dcols = ['column1', 'column2',
@@ -96,8 +108,8 @@ class ActivitesApp:
         self.treeview1.heading('column6', anchor='w', text='Price')
         self.treeview1.grid(column='0', columnspan='5', row='4')
 
-        self.frame2.configure(height='480', width='640')
-        self.frame2.pack(side='top')
+        self.frame1.configure(height='480', width='640')
+        self.frame1.pack(side='top')
         self.toplevel1.configure(height='200', width='200')
 
         # Main widget
@@ -106,10 +118,17 @@ class ActivitesApp:
         populate_list(self)
 
     def record(self):
-        print("Record")
+        db.insert(self.date_entry.get(), self.symbol_entry.get(),
+                  self.transaction_type_combobox.get(), self.quantity_entry.get(), self.price_entry.get())
+        print("done")
+        populate_list(self)
 
     def clear(self):
-        print("clear")
+        self.date_entry.delete('0', 'end')
+        self.symbol_entry.delete('0', 'end')
+        self.transaction_type_combobox.delete('0', 'end')
+        self.quantity_entry.delete('0', 'end')
+        self.price_entry.delete('0', 'end')
 
     def search(self):
         print("search")
